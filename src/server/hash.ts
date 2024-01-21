3649e4a8227e7518512b9108f29c7556eb5c708d
Julian Hubatsch <85513960+jln13x@users.noreply.github.com>
2024-01-21 15:29:10 +0100

    remove unused files

--- src/server/hash.ts
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export const hash = (value: string) => {
-  return crypto.createHash("sha256").update(value, "utf8").digest("hex");
-};
--- src/server/uploadthing.ts
+++
@@ -1,7 +0,0 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-
-const _ = createUploadthing();
-
-export const fileRouter = {} satisfies FileRouter;
-
-export type OurFileRouter = typeof fileRouter;
