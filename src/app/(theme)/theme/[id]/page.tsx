085cc0fc9b876d66b485fff5ea078224c3439a92
Julian Hubatsch <85513960+jln13x@users.noreply.github.com>
2024-01-21 17:26:36 +0100

    meta

--- src/app/(theme)/theme/[id]/page.tsx
+++ src/app/(theme)/theme/[id]/page.tsx
@@ -1,8 +1,12 @@
+import { type Metadata } from "next";
 import { redirect } from "next/navigation";
 
 import { HydrateTheme } from "@/client/components/hydrate-theme";
 import { ThemePage } from "@/client/components/theme-page";
+import { type Hsl } from "@/shared/theme-config";
 import { api } from "@/trpc/server";
+
+import { Colord } from "colord";
 
 type Props = {
   params: {
@@ -10,10 +14,44 @@ type Props = {
   };
 };
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const themeId = params.id;
+
+  const theme = await api.theme.byId.query({
+    id: themeId,
+  });
+
+  if (!theme) {
+    return redirect("/");
+  }
+
+  const hslToHex = (hsl: Hsl) => {
+    return new Colord(hsl).toHex();
+  };
+
+  const primary = hslToHex(theme.config.light.primary);
+  const secondary = hslToHex(theme.config.light.secondary);
+  const accent = hslToHex(theme.config.light.accent);
+
+  const title = `${theme.name} - Theme for shadcn/ui`;
+  const description = `The theme "${theme.name}" consist of the colors ${primary}, ${secondary}, and ${accent}.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      description,
+      title,
+    },
+    twitter: {
+      title,
+      description,
+    },
+  };
+}
+
 const Page = async (props: Props) => {
   const themeId = props.params.id;
-
-  if (!themeId) return redirect("/");
 
   const theme = await api.theme.byId.query({
     id: themeId,
