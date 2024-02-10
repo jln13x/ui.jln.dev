c1f1de1450dbf5b581df9a72d4ea4b3126fea643
Julian Hubatsch <85513960+jln13x@users.noreply.github.com>
2024-02-10 15:55:46 +0100

    support

--- src/client/components/headline.tsx
+++ src/client/components/headline.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import * as Icons from "@/client/components/icons";
 import { Logo } from "@/client/components/logo";
 import { Badge } from "@/client/components/ui/badge";
 import { cn } from "@/client/lib/cn";
 
-import { MousePointerClick } from "lucide-react";
+import { Coffee } from "lucide-react";
 
 export const Headline = () => {
   return (
@@ -35,15 +36,29 @@ export const Headline = () => {
         Themes for shadcn/ui
       </h1>
 
-      <a
-        href="https://x.com/jlndev"
-        className="flex items-center gap-2 rounded border border-accent-foreground/20 bg-accent  px-4 py-0.5 text-sm text-accent-foreground"
-      >
-        <span>
-          Built by <span className="font-bold">jlndev</span>
-        </span>
-        <MousePointerClick className="size-4" />
-      </a>
+      <div className="pt-6">
+        <div className="flex flex-col items-center gap-1.5 text-sm text-muted-foreground">
+          <p>Found an awesome theme? Consider supporting me.</p>
+          <div className="flex items-center gap-2">
+            <a
+              href="https://x.com/jlndev"
+              className="flex items-center gap-2 rounded border border-accent-foreground/20 bg-accent  px-4 py-0.5 text-sm text-accent-foreground"
+            >
+              <Icons.Xcom className="size-4" />
+              <span className="font-medium">
+                Built by <span className="font-bold">jlndev</span>
+              </span>
+            </a>
+            <a
+              href="https://www.buymeacoffee.com/jlndev"
+              className="flex items-center gap-1 rounded border border-muted-foreground/20 bg-muted px-4 py-0.5 font-medium text-muted-foreground "
+            >
+              <Coffee className="size-4" />
+              Buy me a coffee
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
