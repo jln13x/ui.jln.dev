1a6f1736dcec30305162a62eee35b397543e55d4
Julian Hubatsch <85513960+jln13x@users.noreply.github.com>
2024-02-15 22:30:25 +0100

    previous

--- src/client/components/menu/go-back.tsx
+++ src/client/components/menu/go-back.tsx
@@ -3,11 +3,6 @@ import Link from "next/link";
 import * as Icons from "@/client/components/icons";
 import { MenuButton } from "@/client/components/menu/menu-button";
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/client/components/ui/tooltip";
-import {
   themeStackAtom,
   useSetThemeConfig,
 } from "@/client/lib/use-theme-config";
@@ -21,29 +16,24 @@ export const GoBack = () => {
   if (stack.length === 0) return null;
 
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <MenuButton asChild>
-          <Link
-            href="/"
-            onClick={async () => {
-              const theme = stack.pop();
-              setStack(stack);
+    <MenuButton asChild>
+      <Link
+        href="/"
+        onClick={async () => {
+          const theme = stack.pop();
+          setStack(stack);
 
-              if (theme) {
-                setTheme(theme, false);
-                return;
-              }
-            }}
-            className="flex h-8 items-center gap-2 px-6 py-1.5 text-xs"
-            scroll={false}
-          >
-            <Icons.Previous className="size-4" />
-            <span className="sr-only">Previous</span>
-          </Link>
-        </MenuButton>
-      </TooltipTrigger>
-      <TooltipContent>Back to previous theme</TooltipContent>
-    </Tooltip>
+          if (theme) {
+            setTheme(theme, false);
+            return;
+          }
+        }}
+        className="flex h-8 items-center gap-2 px-6 py-1.5 text-xs"
+        scroll={false}
+      >
+        <Icons.Previous className="size-4" />
+        <span>Previous</span>
+      </Link>
+    </MenuButton>
   );
 };
