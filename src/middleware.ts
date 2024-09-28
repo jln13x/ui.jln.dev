5273ab7c86064d27d1a9895a99940abbf38e8249
jln <85513960+jln13x@users.noreply.github.com>
2024-09-28 11:55:24 +0200

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,5 +35,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next|u/|).*)", "/", "/(api|trpc)(.*)"],
 };
